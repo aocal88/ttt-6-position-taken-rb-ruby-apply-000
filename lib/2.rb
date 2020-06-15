50ad3c18705e274c20f6ae5f644e6099ca66e1e0
HAN O <a.ocal88@gmail.com>
2020-06-15 20:34:40 +0000

    Automatically backed up by Learn

--- lib/2.rb
+++ lib/2.rb
@@ -9,12 +9,3 @@ def position_taken?(board, index_number)
   end
 end
 
-puts position_taken?(board, 1)
-puts position_taken?(board, 2)
-puts position_taken?(board, 3)
-puts position_taken?(board, 4)
-puts position_taken?(board, 5)
-puts position_taken?(board, 6)
-puts position_taken?(board, 7)
-puts position_taken?(board, 8)
-puts position_taken?(board, 9)
