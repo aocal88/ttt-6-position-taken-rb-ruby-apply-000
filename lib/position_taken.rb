cdcb0616c48080115106967259d89ec372101f44
HAN O <a.ocal88@gmail.com>
2020-06-15 20:40:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 
-def position_taken?(board, index_number)
+def position_taken?(board, index)
   index == "X" || index == "O"
   index != "" && index != " " && index != nil
 end
