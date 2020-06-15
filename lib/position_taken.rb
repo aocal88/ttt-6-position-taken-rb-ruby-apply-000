e838b761813ffc4a10ccdb2b809e604bbe7ff948
HAN O <a.ocal88@gmail.com>
2020-06-15 20:40:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,14 +5,3 @@ def position_taken?(board, index)
   index == "X" || index == "O"
   index != "" && index != " " && index != nil
 end
-
-
-puts position_taken?(board, 1)
-puts position_taken?(board, 2)
-puts position_taken?(board, 3)
-puts position_taken?(board, 4)
-puts position_taken?(board, 5)
-puts position_taken?(board, 6)
-puts position_taken?(board, 7)
-puts position_taken?(board, 8)
-puts position_taken?(board, 9)
