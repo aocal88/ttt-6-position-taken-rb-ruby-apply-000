20742cef33209af05a45b5d030fb3ea179cc41d3
HAN O <a.ocal88@gmail.com>
2020-06-15 20:38:21 +0000

    Automatically backed up by Learn

--- lib/2.rb
+++ lib/2.rb
@@ -1,7 +1,6 @@
 
 
 def position_taken?(board, index_number)
-  index = board[index_number.to_i - 1]
   if index == "X" || index == "O"
     true
   elsif index == "" || index == " " || index == nil
